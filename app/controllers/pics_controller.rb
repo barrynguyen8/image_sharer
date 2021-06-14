9a440cfbb1cc2060aeb849eca85c64e045368bfb
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-06-14 13:45:43 +0000

    create new Pics actions and view files

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,4 +1,19 @@
 class PicsController < ApplicationController
     def index
     end 
+    
+    def new #adds new images 
+        @pic = Pic.new 
+    end 
+    
+    def create #saving it to database
+        @pic = Pic.new(pic_params)
+    end 
+    
+    private
+    
+    def pic_params
+        params.require(:pic).permit(:title, :description)
+    end 
+    
 end
