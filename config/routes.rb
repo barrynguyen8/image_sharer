c2acd8448dfb2b5c6c41d68700cc82c626f6e116
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-06-14 13:25:10 +0000

    add model and controller for Pics

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    resources :pics #creates all the routes of the views files for CRUD actions
+    
+    root "pics#index"
+
 end
